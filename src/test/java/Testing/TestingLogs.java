44f6f9917e1e278780e682ae7c7a0ebfd64d7319
Gvo3d <denis.yakimov89@gmail.com>
2016-01-09 17:32:38 +0200

    Ver 7 (working) - refactored, with plain file logging, working and testing with spring and junit4

--- src/test/java/Testing/TestingLogs.java
+++ src/test/java/Testing/TestingLogs.java
@@ -1,14 +1,18 @@
-package logger;
+package Testing;
 
+import logger.LogSystem;
+import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Gvozd on 09.01.2016.
  */
-public class App {
-    public static void main(String[] args) {
+public class TestingLogs {
+
+    @Test
+    public void testLog(){
         ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"config.xml"});
         LogSystem loggersystem = (LogSystem) ac.getBean("loggersystem");
-        loggersystem.write("SpringFrawork tested");
+        loggersystem.write("LogSystem testing with JUnit4 and Spring Framework");
     }
 }
