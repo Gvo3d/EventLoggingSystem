5112b96a51f7185af9d0b9a53fb586a688194792
Gvo3d <denis.yakimov89@gmail.com>
2016-01-09 16:58:59 +0200

    Ver 6.1 (working) - refactored, with plain gile logging, trying to make it work with spring

--- src/main/java/Logger/App.java
+++ src/main/java/Logger/App.java
@@ -1,4 +1,4 @@
-package Logger;
+package logger;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,5 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(new String[] {"config.xml"});
+        LogSystem loggersystem = (LogSystem) ac.getBean("loggersystem");
+        loggersystem.write("Event FUUUUUUUUUUUUUUCK");
     }
 }
